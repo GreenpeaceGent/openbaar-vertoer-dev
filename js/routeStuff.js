423bc3624e58e70694cccaec7475b0f44a4f8345
GreenpeaceGent <96986956+GreenpeaceGent@users.noreply.github.com>
2022-01-13 00:20:52 +0100

    instructie overlay

---
+++ js/routeStuff.js
@@ -0,0 +1,16 @@
+
+function showBusRoute(){
+document.getElementById("bus-route-overlay").style.display = "block";
+}
+
+function showBikeRoute(){
+document.getElementById("bike-route-overlay").style.display = "block";
+}
+
+function closeBusRouteOverlay(){
+document.getElementById("bus-route-overlay").style.display = "none";
+}
+
+function closeBikeRouteOverlay(){
+document.getElementById("bike-route-overlay").style.display = "none";
+}
